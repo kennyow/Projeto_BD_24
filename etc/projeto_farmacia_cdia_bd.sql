7617fe233c310527d8ca84e85558695b5507dd48
kennyow <kennyow86@gmail.com>
2024-04-25 22:52:58 -0300

    Create Clientes table

--- etc/projeto_farmacia_cdia_bd.sql
+++ etc/projeto_farmacia_cdia_bd.sql
@@ -2,8 +2,8 @@
 CREATE DATABASE farmacia;
 USE farmacia;
 
-CREATE TABLE medicamentos(
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS medicamentos(
+    id INT NOT NULL AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     categoria_regulatoria VARCHAR(40) NOT NULL,
     data_vencimento_registro DATE,
@@ -12,10 +12,31 @@ CREATE TABLE medicamentos(
     principio_ativo VARCHAR(400) NOT NULL,
     preco DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id)
-    );
-    
-SELECT * FROM medicamentos;
+);
 
+CREATE TABLE IF NOT EXISTS clientes(
+    idcliente INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(50) NOT NULL,
+    telefone VARCHAR(10) NOT NULL,
+    endereco VARCHAR(70) NOT NULL,
+    cidade VARCHAR(50) NOT NULL,
+    isflamengo CHAR(1) NOT NULL,
+    isonepiece CHAR(1) NOT NULL,
+    issousa CHAR(1) NOT NULL
+);
+
+INSERT INTO clientes (nome, telefone, endereco, cidade, isflamengo, isonepiece, issousa) VALUES
+('Luke Skywalker', '1234567890', 'Tatooine', 'Mos Eisley', 'N', 'Y', 'N'),
+('Leia Organa', '9876543210', 'Alderaan', 'Rebel Base', 'N', 'N', 'N'),
+('Han Solo', '5551234567', 'Millennium Falcon', 'Corellia', 'Y', 'N', 'N'),
+('Darth Vader', '9998887776', 'Death Star', 'Unknown', 'N', 'N', 'N'),
+('Obi-Wan Kenobi', '1112223334', 'Jedi Temple', 'Coruscant', 'N', 'Y', 'N'),
+('Yoda', '7778889990', 'Dagobah', 'Swampy Planet', 'N', 'N', 'N'),
+('Padmé Amidala', '3334445556', 'Naboo', 'Theed', 'N', 'N', 'N'),
+('Chewbacca', '8887776665', 'Kashyyyk', 'Wookiee Planet', 'N', 'N', 'N'),
+('R2-D2', '2223334447', 'N/A', 'Outer Space', 'N', 'N', 'N'),
+('C-3PO', '4445556663', 'N/A', 'Outer Space', 'N', 'N', 'N'),
+('Mace Windu', '6667778889', 'Jedi Temple', 'Coruscant', 'N', 'N', 'Y');
 -- COMANDOS UTILIZADOS NO RELATÓRIO 
 
 -- Quantidade de Medicamentos
